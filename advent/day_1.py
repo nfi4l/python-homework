2ad0ed0bbf698afc43150ba3b289b62046b954ef
Nathalie Fial <n.fial1998@gmail.com>
2024-12-03 10:24:38 +0100

    added day 1

--- advent/day_1.py
+++ advent/day_1.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-#import file with two columns
+def count_occurences (x, l):
+    counter = 0
+    for num in l:
+        if x == num:
+            counter += 1
+    return counter
+
+# import file with two columns
 with open("input/input_1.txt", "r") as file:
     locID1 = []
     locID2 = []
@@ -11,13 +18,13 @@ with open("input/input_1.txt", "r") as file:
         locID1.append(int(num1))
         locID2.append(int(num2))
 
-#pair up -> measure differences
+# Part 1:
+# pair up -> measure differences
+# sort
+s_loc1 = sorted(locID1) #or use locID1.sort()
+s_loc2 = sorted(locID2)
 
-#sort
-s_loc1 = sorted(locID1)
-s_loc2= sorted(locID2)
-
-#calculate distance
+# calculate distance
 distances = []
 
 for a, b in zip(s_loc1, s_loc2):
@@ -28,3 +35,18 @@ for a, b in zip(s_loc1, s_loc2):
 # print(*distances)
 print("total distance:")
 print(sum(distances))
+
+# Part 2:
+
+# figure out exactly how often each number from the left list appears in the right list -> count occurences
+# calculate similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+
+col1 = locID1
+col2 = locID2
+similarity_index = []
+
+for a in col1:
+    occ_a = count_occurences(a, col2)
+    similarity_index.append(a * occ_a)
+
+print(sum(similarity_index))
